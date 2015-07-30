e270b4e64eb584f44df02f05998d39b7a3bd73d9
Noah Finer <nfiner0149@gmail.com>
2015-07-29 21:30:06 -0400

    Revert "Revert "Cross-browser support and final touches""
    This reverts commit cbd7197fb99cc0c427789b7962e42f9d34fa464d.

--- photos.js
+++ photos.js
@@ -50,23 +50,23 @@ $(document).ready(function() {
   var c3 = new Photo(3, "photos/3.jpg", "Waiting for Bee", "11.5x15.5", "digital");
   var c4 = new Photo(4, "photos/4.jpg", "Ominous", "14x16", "digital");
   var c5 = new Photo(5, "photos/5.jpg", "Pushing Through", "16x14", "digital");
-  var c6 = new Photo(6, "photos/6.jpg", "Spring", "16x14", "digital");
+  var c6 = new Photo(6, "photos/6.jpg", "Watching His World", "14x14", "digital");
   var c7 = new Photo(7, "photos/7.jpg", "Untitled", "16x14", "digital");
   var c8 = new Photo(8, "photos/8.jpg", "All Ears", "14x16", "digital");
   var c9 = new Photo(9, "photos/9.jpg", "Hide and Seek", "14x16", "digital");
   var c10 = new Photo(10, "photos/10.jpg", "Two Toned", "14x16", "digital");
-  var c11 = new Photo(11, "photos/11.jpg", "NYNY", "10.5x18", "digital");
+  var c11 = new Photo(11, "photos/11.jpg", "River View", "10.5x18", "digital");
   var c12 = new Photo(12, "photos/12.jpg", "Blue Ice", "14x16", "digital");
   var c13 = new Photo(13, "photos/13.jpg", "Beyond", "14x16", "digital");
   var c14 = new Photo(14, "photos/14.jpg", "Star Catcher", "14x16", "digital");
   var c15 = new Photo(15, "photos/15.jpg", "Autunm", "14x16", "digital");
-  var c16 = new Photo(16, "photos/16.jpg", "Pool in the Ice", "14x16", "digital");
+  var c16 = new Photo(16, "photos/16.jpg", "Blue Pool", "14x16", "digital");
   var c17 = new Photo(17, "photos/17.jpg", "The Seashelf", "14x16", "digital");
   var c18 = new Photo(18, "photos/18.jpg", "Feathers", "14x16", "digital");
   var c19 = new Photo(19, "photos/19.jpg", "Temple in the Rain", "14x16", "digital");
   var c20 = new Photo(20, "photos/20.jpg", "Window to What", "16x14", "digital");
   var c21 = new Photo(21, "photos/21.jpg", "Pot Of Gold", "16x14", "digital");
-  var c22 = new Photo(22, "photos/22.jpg", "Arriving Surf", "12x16", "digital");
+  var c22 = new Photo(22, "photos/22.jpg", "Magic Morning", "12x16", "digital");
   var c23 = new Photo(23, "photos/23.jpg", "Spring Awakening", "12x16", "digital");
   var c24 = new Photo(24, "photos/24.jpg", "Noah's Wall", "16x16", "digital");
 
